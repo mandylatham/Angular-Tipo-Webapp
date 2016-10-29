37d20d7f602eb0b36087982d72aeeba04dc4bc13
mandylatham <mandylthm@gmail.com>
2016-10-29 10:45:07 +1000

    New dev URL with modified API gateway implementation.

--- src/express-server.dev.js
+++ src/express-server.dev.js
@@ -17,7 +17,8 @@ var pathMappings = [
   {
     path: '/dev',
     //url: 'http://localhost:9001'
-    url: 'https://k2lajprh2f.execute-api.us-east-1.amazonaws.com'
+    //url: 'https://k2lajprh2f.execute-api.us-east-1.amazonaws.com'
+    url: 'https://vjcw1m6555.execute-api.us-east-1.amazonaws.com'
   }
 ];
 
