cd6b6224cd36267e4769a43932ce146ede2c29e0
mandylatham <mandylthm@gmail.com>
2016-11-06 17:10:38 +0200

    Introduce s3explorer directive

---
+++ src/app/_scripts/S3Service.js
@@ -0,0 +1,58 @@
+(function() {
+
+  'use strict';  
+
+  function S3Service($q) {
+
+    var s3 = new AWS.S3();
+    var scope = {
+        Contents: [], CommonPrefixes:[], params: {}, stop: false, completecb: null
+    };
+
+    function cb(err, data) {
+        if (err) { 
+            console.log('Error: ' + JSON.stringify(err));
+            console.log('Error: ' + err.stack);
+        } else {
+            console.log(data);
+        }
+        // Filter the folders out of the listed S3 objects
+        // (could probably be done more efficiently)
+        console.log("Filter: remove folders");
+        data.Contents = data.Contents.filter(function(el) {
+            return el.Key !== scope.params.Prefix;
+        });
+
+        // Accumulate the S3 objects and common prefixes
+        scope.Contents.push.apply(scope.Contents, data.Contents);
+        scope.CommonPrefixes.push.apply(scope.CommonPrefixes, data.CommonPrefixes);
+
+        console.log('Bucket ' + scope.params.Bucket + ' has ' + scope.Contents.length + ' objects, including ' + scope.CommonPrefixes.length + ' prefixes');
+        delete scope.params.Marker;
+        if (scope.completecb) {
+            scope.completecb(scope, true);
+        }
+    }
+
+    function go(config, completecb = null) {
+      scope.completecb = completecb;
+      scope.params = { Bucket: config.Bucket, Prefix: config.Prefix, Delimiter: config.Delimiter };
+      //$('#bucket-loader').addClass('fa-spin');
+      //$('#tb-s3objects').DataTable().clear();
+      s3.makeUnauthenticatedRequest('listObjects', scope.params, cb);
+    }
+
+    function stop() {
+    
+    }
+
+    return {
+      go: go,
+      stop: stop
+    };
+  }
+
+  angular.module('tipo.main')
+    .service('s3Service', S3Service);
+
+})();
---
+++ src/app/framework/_directives/_scripts/tpS3Explorer.js
@@ -0,0 +1,29 @@
+(function () {
+
+  'use strict';
+
+  var s3exp_config = { Region: '', Bucket: '', Prefix: '', Delimiter: '/' };
+  var s3exp_lister = null;
+  var s3exp_columns = { key:1, folder:2, date:3, size:4 };
+
+  AWS.config.region = 'us-east-1';
+  console.log('Region: ' + AWS.config.region);
+  
+  s3exp_config.Bucket = 'fadeev.yegor';
+
+  var module = angular.module('tipo.framework');
+  return module.directive('tpS3Explorer', function (s3Service) {
+      return {
+        templateUrl: 'framework/_directives/_views/tp-s3-explorer.tpl.html',
+        link: function($scope, element, attrs) {
+            s3Service.go(s3exp_config, function s3draw(data, complete) {
+                $scope.$apply(function () {
+                    $scope.rows = data.Contents;
+                });                
+                console.log($scope.rows);
+            });
+        }
+      };
+    }
+  );
+})();
