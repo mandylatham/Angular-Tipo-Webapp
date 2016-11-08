4817cbdd452809f4d81b04e4f8a563b179bbd0db
mandylatham <mandylthm@gmail.com>
2016-11-08 16:14:45 +0200

    Enable ui-grid in tipodef view/edit mode

--- src/app/framework/_directives/_scripts/tpS3Explorer.js
+++ src/app/framework/_directives/_scripts/tpS3Explorer.js
@@ -11,18 +11,54 @@
   
   s3exp_config.Bucket = 'fadeev.yegor';
 
+  function object2hrefvirt(bucket, object) {
+      if (AWS.config.region === "us-east-1") {
+          return document.location.protocol + '//' + bucket + '.s3.amazonaws.com/' + object;
+      } else {
+          return document.location.protocol + '//' + bucket + '.s3-' + AWS.config.region + '.amazonaws.com/' + object;
+      }
+  }
+
+  // Convert cars/vw/golf.png to golf.png
+  function fullpath2filename(path) {
+      return path.replace(/^.*[\\\/]/, '');
+  }
+
   var module = angular.module('tipo.framework');
   return module.directive('tpS3Explorer', function () {
       return {
-        restrict: 'E',
         templateUrl: 'framework/_directives/_views/tp-s3-explorer.tpl.html',
-        replace : true,
-        transclude : false,
         controller: controller
       };
       
       function controller($scope, s3Service) {
-        $scope.gridOptions = {};
+        function renderObject(data) {
+            if (isfolder(data)) {
+                console.log("is folder: " + data);
+                return '<a data-s3="folder" data-prefix="' + data + '" href="' + object2hrefvirt(s3exp_config.Bucket, data) + '">' + prefix2folder(data) + '</a>';
+            } else {
+                console.log("not folder/this document: " + data);
+                return '<a data-s3="object" href="' + object2hrefvirt(s3exp_config.Bucket, data) + '">' + fullpath2filename(data) + '</a>';
+            }
+        }
+
+        $scope.gridOptions = {
+            enableRowSelection: true,
+            columnDefs: [
+                { 
+                    field: 'Key',
+                    cellTemplate: '<div class="ui-grid-cell-contents" >{{grid.getCellValue(row, col)}}</div>'
+                },
+                { 
+                    field: 'LastModified'
+                    //cellTemplate: ''
+                },
+                { 
+                    field: 'Size',
+                    //cellTemplate: ''
+                }
+            ]
+        };
         $scope.gridOptions.data = [];
         s3Service.go(s3exp_config, function s3draw(data, complete) {
             var rows = data.CommonPrefixes.map(function(prefix) {
@@ -32,17 +68,9 @@
                     Size: null
                 };
             });
-            // rows = rows.concat(data.Contents);
-            // $scope.rows = rows;
-
-            var myData = [{name: "Moroni", age: 50},
-                {name: "Tiancum", age: 43},
-                {name: "Jacob", age: 27},
-                {name: "Nephi", age: 29},
-                {name: "Enos", age: 34}];
-            
+            rows = rows.concat(data.Contents);
             $scope.$apply(function () {
-                $scope.gridOptions.data = myData;    
+                $scope.gridOptions.data = rows;    
             });                
             console.log($scope.rows);
         });
