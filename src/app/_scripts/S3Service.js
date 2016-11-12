1cc4febebe29237711afa5c7d44170f4662beb37
mandylatham <mandylthm@gmail.com>
2016-11-13 01:39:25 +0200

    Add progressbar and select new bucket popup

--- src/app/_scripts/S3Service.js
+++ src/app/_scripts/S3Service.js
@@ -9,48 +9,38 @@
         params: {}, stop: false, completecb: null
     };
 
-    function cb(err, data) {
-        if (err) { 
-            console.log('Error: ' + JSON.stringify(err));
-            console.log('Error: ' + err.stack);
-        } else {
-            console.log(data);
-        }
-        // Filter the folders out of the listed S3 objects
-        // (could probably be done more efficiently)
-        console.log("Filter: remove folders");
-        data.Contents = data.Contents.filter(function(el) {
-            return el.Key !== scope.params.Prefix;
-        });
-
-        console.log('Bucket ' + scope.params.Bucket + ' has ' + data.Contents.length + ' objects, including ' + data.CommonPrefixes.length + ' prefixes');
-        
-        if (scope.completecb) {
-            var cbScope = {
-                Contents: data.Contents.slice(),
-                CommonPrefixes: data.CommonPrefixes.slice(),
-                params: { Bucket: scope.params.Bucket, Prefix: scope.params.Prefix, Delimiter: scope.params.Delimiter }
-            };
-            
-            scope.completecb(cbScope, true);
-        }
-    }
-
     function go(config, completecb) {
-      scope.completecb = completecb;
-      scope.params = { Bucket: config.Bucket, Prefix: config.Prefix, Delimiter: config.Delimiter };
-      //$('#bucket-loader').addClass('fa-spin');
-      //$('#tb-s3objects').DataTable().clear();
-      s3.makeUnauthenticatedRequest('listObjects', scope.params, cb);
-    }
+        scope.params = { Bucket: config.Bucket, Prefix: config.Prefix, Delimiter: config.Delimiter };
+        var deferred = $q.defer();
+        s3.makeUnauthenticatedRequest('listObjects', scope.params, function cb(err, data) {
+            if (err) { 
+                console.log('Error: ' + JSON.stringify(err));
+                console.log('Error: ' + err.stack);
+                deferred.reject(err);
+                return;
+            } 
 
-    function stop() {
-    
+            // Filter the folders out of the listed S3 objects
+            // (could probably be done more efficiently)
+            data.Contents = data.Contents.filter(function(el) {
+                return el.Key !== scope.params.Prefix;
+            });
+
+            if (completecb) {
+                var cbScope = {
+                    Contents: data.Contents.slice(),
+                    CommonPrefixes: data.CommonPrefixes.slice(),
+                    params: { Bucket: scope.params.Bucket, Prefix: scope.params.Prefix, Delimiter: scope.params.Delimiter }
+                };
+                var result = completecb(cbScope, deferred);
+                deferred.resolve(result);
+            }
+        });
+        return deferred.promise;
     }
 
     return {
-      go: go,
-      stop: stop
+      go: go
     };
   }
 
--- src/app/framework/_directives/_scripts/tpS3Explorer.js
+++ src/app/framework/_directives/_scripts/tpS3Explorer.js
@@ -4,8 +4,7 @@
 
   AWS.config.region = 'us-east-1';
   var s3exp_config = { Region: '', Bucket: '', Prefix: '', Delimiter: '/' };
-  
-  s3exp_config.Bucket = 'fadeev.yegor';
+  s3exp_config.Bucket = '<bucket>';
 
   function object2hrefvirt(bucket, object) {
       if (AWS.config.region === "us-east-1") {
@@ -29,6 +28,39 @@
       var parts = prefix.split('/');
       return parts[parts.length-2] + '/';
   }
+
+  // The parts array will contain the bucket name followed by all the
+  // segments of the prefix, exploded out as separate strings.
+  function folder2breadcrumbs(data) {
+      var parts = [];
+      if (data.params.Prefix) {
+          parts = data.params.Prefix.endsWith('/') ?
+              data.params.Prefix.slice(0, -1).split('/') :
+              data.params.Prefix.split('/');
+      }
+
+      var buildprefix = '';
+      var breadcrumbs = [{
+          name: data.params.Bucket + '/',
+          prefix: buildprefix
+      }];
+
+      parts.forEach(function(part) {
+          buildprefix += part + '/';
+          breadcrumbs.push({
+              name: part + ' /',
+              prefix: buildprefix
+          });
+      });
+      return breadcrumbs;
+  }
+
+  function bytesToSize(bytes) {
+      var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+      if (bytes === 0) return '0 Bytes';
+      var ii = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+      return Math.round(bytes / Math.pow(1024, ii), 2) + ' ' + sizes[ii];
+  }
   
   var module = angular.module('tipo.framework');
   return module.directive('tpS3Explorer', function () {
@@ -37,36 +69,15 @@
         controller: controller
       };
       
-      function controller($scope, s3Service) {
-        function folder2breadcrumbs(data) {
-            console.log('Prefix: ' + data.params.Prefix);
-            // The parts array will contain the bucket name followed by all the
-            // segments of the prefix, exploded out as separate strings.
-            var parts = [];
-            if (data.params.Prefix) {
-                parts = data.params.Prefix.endsWith('/') ?
-                            data.params.Prefix.slice(0, -1).split('/') :
-                            data.params.Prefix.split('/');
-            }
+      function controller($scope, $mdDialog, s3Service) {
 
-            var buildprefix = '';
-            var breadcrumbs = [{
-                name: data.params.Bucket,
-                prefix: buildprefix
-            }];
-           
-            for (var i = 0; i < parts.length; i++) {
-                var part = parts[i];
-                buildprefix += part + '/';
-                breadcrumbs.push({
-                    name: part,
-                    prefix: buildprefix
-                });
-            }
-
-            return breadcrumbs;
+        function retryFailure(promise) {
+            promise.then(function(result){
+            }, function(err) {
+                $scope.chooseBucket();
+            });
         }
-        
+
         function s3draw(data, complete) {
             var breadcrumbs = folder2breadcrumbs(data);
             
@@ -83,37 +94,51 @@
             var objects = data.Contents.map(function(object) {
                 return {
                     Key: fullpath2filename(object.Key),
-                    LastModified: object.LastModified,
-                    Size: object.Size,
+                    LastModified: moment(object.LastModified).fromNow(),
+                    Size: bytesToSize(object.Size),
                     s3: 'object',
                     prefix: null,
                     href: object2hrefvirt(s3exp_config.Bucket, object.Key)
                 };
             });
             var rows = prefixes.concat(objects);
-            console.log(rows);
             $scope.$apply(function () {
                 $scope.rows = rows; 
                 $scope.breadcrumbs = breadcrumbs;
             });
+            return rows;
         }
         
-        $scope.selectObject = function(target) {
-            console.log('Selected target ' + target.Key);
-            if (target.s3 === "folder") {
-                //delete s3exp_config.Marker;
-                s3exp_config.Prefix = target.prefix;
-                //s3exp_config.Delimiter = $("input[name='optionsdepth']:checked").val() == "folder" ? "/" : "";
-                s3Service.go(s3exp_config, s3draw);
-            // Else user has clicked on an object so download it in new window/tab
+        $scope.selectObject = function(obj) {
+            if (obj.s3 === 'folder') {
+                s3exp_config.Prefix = obj.prefix;
+                $scope.promise = s3Service.go(s3exp_config, s3draw);
+                retryFailure($scope.promise);
             } else {
-                window.open(target.href, '_blank');
+                // Else user has clicked on an object
+                console.log('Selected object: ' + obj.href);
             }
         }
 
         $scope.selectBreakcrumb = function(breakcrumb) {
-            var config = {Bucket: s3exp_config.Bucket, Prefix: breakcrumb.prefix, Delimiter: s3exp_config.Delimiter};
-            s3Service.go(config, s3draw);
+            var config = { Bucket: s3exp_config.Bucket, Prefix: breakcrumb.prefix, Delimiter: s3exp_config.Delimiter };
+            $scope.promise = s3Service.go(config, s3draw);
+            retryFailure($scope.promise);
+        }
+
+        $scope.chooseBucket = function() {
+            var confirm = $mdDialog.prompt()
+                .title('Please enter the S3 bucket name')
+                .textContent('(Use s3 bucket "fadeev.yegor")')
+                .initialValue('<bucket>')
+                .targetEvent(null)
+                .ok('OK')
+                .cancel('Cancel');
+            $mdDialog.show(confirm).then(function(result) {
+                s3exp_config.Bucket = result;
+                $scope.promise = s3Service.go(s3exp_config, s3draw);
+                retryFailure($scope.promise);
+            });
         }
 
         $scope.selected = [];
@@ -122,8 +147,8 @@
             limit: 5,
             page: 1
         };
-
-        s3Service.go(s3exp_config, s3draw);
+        $scope.promise = s3Service.go(s3exp_config, s3draw);
+        retryFailure($scope.promise);
       }
   });
 })();
