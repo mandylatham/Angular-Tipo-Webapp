8a6b0de25aafdc93ba0cbcfae6907a90f7853bd2
mandylatham <mandylthm@gmail.com>
2016-12-21 09:59:58 +0200

    Display default title in case of an error

--- src/app/dashboard/_scripts/DashboardController.js
+++ src/app/dashboard/_scripts/DashboardController.js
@@ -6,15 +6,15 @@
     $scope,
     $location,
     cognitoService) {
-        
 
     function verifyConfirmationCode() {
       var params = $location.search();
       if (params.code) {
         cognitoService.verifyCode(params.code).then(function() {
-            $scope.title = 'Email has been confirmed';
+          $scope.title = 'Email has been confirmed';
         }, function(err) {
-            console.error(err);
+          console.error(err);
+          $scope.title = 'Dashboard';
         });
         return;
       }
