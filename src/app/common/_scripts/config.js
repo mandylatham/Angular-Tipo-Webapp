504ad5f8f87a3f7bac4f50ff91705103b9d523be
mandylatham <mandylthm@gmail.com>
2017-01-17 13:39:41 +1000

    refer to environment variables

--- src/app/common/_scripts/config.js
+++ src/app/common/_scripts/config.js
@@ -1,8 +1,8 @@
 var TIPO_CONSTANTS = {
   COGNITO: {
-    REGION: '${cognito_region}',
-    IDENTITY_POOL_ID: '${cognito_identity_pool_id}',
-    USER_POOL_ID: '${cognito_user_pool_id}',
-    CLIENT_ID: '${cognito_client_id}'
+    REGION: '${env.cognito_region}',
+    IDENTITY_POOL_ID: '${env.cognito_identity_pool_id}',
+    USER_POOL_ID: '${env.cognito_user_pool_id}',
+    CLIENT_ID: '${env.cognito_client_id}'
   }
 };
