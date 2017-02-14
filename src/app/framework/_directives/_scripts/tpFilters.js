01953adb198af18a703fae7e30ab532ab32bc08a
mandylatham <mandylthm@gmail.com>
2017-02-14 14:31:27 +0530

    Added Filters Logic and UI

--- src/app/framework/_directives/_scripts/tpFilters.js
+++ src/app/framework/_directives/_scripts/tpFilters.js
@@ -28,13 +28,27 @@
             }
           };
 
+          if($stateParams.filter){
+            scope.currentExpression = $stateParams.filter;
+            scope.selectedArray = scope.currentExpression.split(" and ");
+            scope.selectedObj = _.zipObject(scope.selectedArray,scope.selectedArray);
+          }else{
+            scope.selectedObj = {};
+            scope.selectedArray = [];
+          }
+
+
           function prepareFilters(){
             var tipoFilters = _.get(scope.definition, 'tipo_list.filters');
-
             var filters = _.map(tipoFilters, function(each){
+              var selected = false;
+              if(scope.selectedObj[each.filter_expression]){
+                selected = true;
+              }
               var filter = {
                 name: each.display_name,
-                expression: each.filter_expression
+                expression: each.filter_expression,
+                selected: selected
               };
               return filter;
             });
@@ -44,19 +58,23 @@
 
           scope.filters = prepareFilters();
 
-          if(!_.isEmpty(scope.filters)){
-            scope.filter = {};
-            scope.$watch('filter.selected', function(newValue, oldValue){
-              if(!_.isUndefined(newValue)){
-                scope.currentExpression = newValue.expression;
+          function removeFromCurrentExpression(filter){
+            scope.selectedArray.splice(scope.selectedArray.indexOf(filter.expression),1);
+            scope.currentExpression = scope.selectedArray.join(" and ");
+          }
+
+          scope.applyFilter = function(filter){
+            if (filter.selected) {
+              removeFromCurrentExpression(filter);
+            }else{
+              if ($stateParams.filter) {
+              scope.currentExpression = scope.currentExpression + " and " + filter.expression;
+              }else{
+                scope.currentExpression = filter.expression;
               }
-            });
+            }        
+            scope.search();
           }
-
-          if($stateParams.filter){
-            scope.currentExpression = $stateParams.filter;
-          }
-
         }
       };
     }
