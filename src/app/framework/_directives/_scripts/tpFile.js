8506ae7ec4a95e3b627b39c53d196449c11aa9a8
mandylatham <mandylthm@gmail.com>
2017-03-13 17:54:38 +0530

    Converting to base64 just in case

--- src/app/framework/_directives/_scripts/tpFile.js
+++ src/app/framework/_directives/_scripts/tpFile.js
@@ -155,15 +155,28 @@
               $scope.upload = function () {
                 if($scope.content.length > 0){
                   $scope.uploadStatus = 'in_progress';
-                  var file = $scope.content[0];
-                  var data = new FormData();
+                  var file = $scope.content[0].lfFile;
+                  /*var data = new FormData();
                   data.append('file', file);
                   tipoResource
                   .oneUrl('content', $scope.finalPath)
                   .customPUT(data, '', undefined, {'Content-Type': undefined})
                   .then(function(result){
                     $scope.uploadStatus = 'completed';
-                  });
+                  });*/
+
+                   var reader = new FileReader();
+                   reader.onload = function(readerEvt) {
+                    var binaryString = readerEvt.target.result;
+                    var base64Encoded = btoa(binaryString);
+                    tipoResource
+                    .oneUrl('content', $scope.finalPath)
+                    .customPUT(base64Encoded, '', undefined, {'Content-Type': undefined, 'isBase64Encoded': true})
+                    .then(function(result){
+                      $scope.uploadStatus = 'completed';
+                    })
+                   };
+                   reader.readAsBinaryString(file);
                 }
               }
 
