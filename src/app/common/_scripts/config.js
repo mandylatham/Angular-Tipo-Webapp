5aedfda61050a07b80e9ee5376ae1e15b9787a4a
mandylatham <mandylthm@gmail.com>
2017-03-31 09:42:45 +1000

    Committed hardcoded values by mistake. Reverting.

--- src/app/common/_scripts/config.js
+++ src/app/common/_scripts/config.js
@@ -1,8 +1,8 @@
 var TIPO_CONSTANTS = {
   COGNITO: {
-          REGION: 'us-east-1',
-          IDENTITY_POOL_ID: 'us-east-1:55edf4db-74d8-42c5-b13b-23269e14584e',
-          USER_POOL_ID: 'us-east-1_ArsTT6ILr',
-          CLIENT_ID: 'h68uh9k3b8vico8b7k65tj378'
+	      REGION: '${env.COGNITO_REGION}',
+	      IDENTITY_POOL_ID: '${env.COGNITO_IDENTITY_POOL_ID}',
+	      USER_POOL_ID: '${env.COGNITO_USER_POOL_ID}',
+	      CLIENT_ID: '${env.COGNITO_CLIENT_ID}'
   }
 };
