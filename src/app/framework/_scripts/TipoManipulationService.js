70ed31fbb1907808a90fdc0020116ab5e65f39db
mandylatham <mandylthm@gmail.com>
2017-04-24 20:04:00 +0530

    Fixed the Null value for boolean type

--- src/app/framework/_scripts/TipoManipulationService.js
+++ src/app/framework/_scripts/TipoManipulationService.js
@@ -463,7 +463,7 @@
     function modifyTipoData(tipoData){
       _.forOwn(tipoData, function(value, key){
         if (!_.isArray(value) && !_.isObject(value)) {
-          if(!_.isUndefined(value) || !_.isEmpty(value)){
+          if(_.isUndefined(value) || _.isEmpty(value)){
             tipoData[key] = null;
           }
         }
