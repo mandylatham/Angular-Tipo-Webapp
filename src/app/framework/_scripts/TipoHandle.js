8d7195a185652659e76015271d5b6a1e476bde8d
mandylatham <mandylthm@gmail.com>
2017-07-28 17:49:05 +0530

    Modified Tipo Handle

--- src/app/framework/_scripts/TipoHandle.js
+++ src/app/framework/_scripts/TipoHandle.js
@@ -6,85 +6,88 @@
   function TipoHandle(tipoCache,
                       tipoInstanceDataService,
                       tipoDefinitionDataService,
+                      $location,
                       $mdToast,
                       $mdDialog){
-    var _instance = this;
+     
+     function getConfirmation(title, user_message){
+       var confirmation = $mdDialog.confirm()
+              .title(title)
+              .textContent(user_message)
+              .ariaLabel(title)
+              .ok('Yes')
+              .cancel('No');
+       $mdDialog.show(confirmation).then(function(){
+          return true;
+       },function(){
+          return false;
+       });
+     }
 
+     function hideElement(element_class){
+      var elem = angular.element(document.querySelector("." + element_class));
+      elem.style.display = 'none';
+     }
 
-    _instance.showMessage = function(user_heading,user_message){
+     function showElement(element_class){
+      var elem = angular.element(document.querySelector("." + element_class));
+      elem.style.display = 'block';
+     }
+
+     function getTipoDefinition(tipo_name){
+      tipoDefinitionDataService.getOne(tipo_name).then(function(definition){
+        return definition;
+      });
+     }
+
+     function callAction(tipo_name, action_name, selected_tipo_ids, additional_tipo_name, additional_tipo){
+      tipoInstanceDataService.performBulkAction(tipo_name,action_name,selected_tipo_ids,additional_tipo_name,additional_tipo).then(function(response){
+        return response;
+      })
+     }
+
+     function routeTo(url){
+      $location.url(url);
+     }
+
+     function saveTipo(tipo_name, tipo_id, tipo_data){
+      tipoInstanceDataService.updateOne(tipo_name,tipo_data,tipo_id).then(function(tipoResponse){
+        return true;
+      });
+     }
+
+     function deleteTipo(tipo_name,tipo_id){
+      tipoInstanceDataService.deleteOne(tipo_name,tipo_id).then(function(tipoResponse){
+        return true;
+      });
+     }
+
+     function getTipo(tipo_name, tipo_id, query_params){
+      tipoCache.evict(tipo_name, tipo_id);
+      tipoInstanceDataService.getOne(tipo_name, tipo_id, query_params).then(function(tipo){
+        return tipo;
+      });
+     }
+
+     function getTipos(tipo_name, query_params){
+      tipoCache.evict(tipo_name);
+      tipoInstanceDataService.search(tipo_name).then(function(tipos){
+        return tipos;
+      });
+     }
+
+     function presentForm(tipo_name, tipo, submit_label, show_cancel){
+
+     }
+
+     function showMessage(user_heading,user_message){
       var toast = $mdToast.tpToast();
       toast._options.locals = {
         header: user_heading,
         body: user_message
       };
       $mdToast.show(toast);
-    };
-
-    _instance.getConfirmation = function(title, user_message){
-      var confirmation = $mdDialog.confirm()
-            .title(title)
-            .textContent(user_message)
-            .ariaLabel(title)
-            .ok('Yes')
-            .cancel('No');
-      $mdDialog.show(confirmation).then(function(){
-        return true;
-      },function(){
-        return false;
-      });
-    }
-
-    _instance.getTipos = function(tipo_name,tipoData,query_params){
-      tipoCache.evict(tipo_name);
-      tipoData = tipoInstanceDataService.search(tipo_name).then(function(tipos){
-        return tipos;
-      });
-    }
-
-    _instance.getTipo = function(tipo_name, tipo_id, tipoData,query_params){
-      tipoCache.evict(tipo_name, tipo_id);
-      tipoData = tipoInstanceDataService.getOne(tipo_name, tipo_id).then(function(tipo){
-        return tipo;
-      });
-    }
-
-    _instance.getTipoDefiniton = function(tipo_name,query_params){
-      tipoDefinitionDataService.getOne(tipo_name).then(function(definition){
-        return definition;
-      });
-    }
-
-    _instance.callAction = function(tipo_name, action, selected_tipo_ids, additional_tipo_name, additional_tipo){
-      var definition = _instance.getDefinition(tipo_name);
-    }
-
-    _instance.routeTo = function(url){
-
-    }
-
-    _instance.search = function(tipo_name,tipoData,query_params){
-
-    }
-
-    _instance.saveTipo = function(tipoData,tipo_name,tipo_id){
-
-    }
-
-    _instance.createTipo = function(tipoData,tipo_name){
-
-    }
-
-    _instance.deleteTipo = function(tipoData,tipo_name,tipo_id){
-
-    }
-
-    _instance.modifyTipoConfig = function(tipo_name){
-
-    }
-
-    _instance.generateTipoConfigs = function(file){
-
-    }
+     };
 
 
 
