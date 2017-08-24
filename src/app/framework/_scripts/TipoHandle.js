e0a8809688505e064062bea84ee945e86ba1da68
mandylatham <mandylthm@gmail.com>
2017-08-24 15:30:17 +0530

    Fixed Change Password

--- src/app/framework/_scripts/TipoHandle.js
+++ src/app/framework/_scripts/TipoHandle.js
@@ -139,7 +139,11 @@
      }
 
      function callAction(tipo_name, action_name, selected_tipo_ids, additional_tipo_name, additional_tipo){
-      return tipoInstanceDataService.performBulkAction(tipo_name,action_name,selected_tipo_ids,additional_tipo_name,additional_tipo);
+      if (selected_tipo_ids.length > 1) {
+        return tipoInstanceDataService.performBulkAction(tipo_name,action_name,selected_tipo_ids,additional_tipo_name,additional_tipo);
+      }else{
+        return tipoInstanceDataService.performSingleAction(tipo_name,selected_tipo_ids[0],action_name,additional_tipo_name,additional_tipo);
+      }
      }
 
      function routeTo(url){
