85d97975d62de533ab39e9b11808a5fa4116bc57
mandylatham <mandylthm@gmail.com>
2017-09-08 12:12:29 +0530

    Fixed Promie issue

--- src/app/framework/_scripts/TipoHandle.js
+++ src/app/framework/_scripts/TipoHandle.js
@@ -146,14 +146,14 @@
      function callAction(tipo_name, action_name, selected_tipo_ids, additional_tipo_name, additional_tipo){
       if (selected_tipo_ids.length > 1) {
         return tipoInstanceDataService.performBulkAction(tipo_name,action_name,selected_tipo_ids,additional_tipo_name,additional_tipo).then(function(response){
-          return response;
           tipoRouter.endStateChange();
+          return response;
         });
       }else{
         return tipoInstanceDataService.performSingleAction(tipo_name,selected_tipo_ids[0],action_name,additional_tipo_name,additional_tipo).then(function(response){
-          return response;
           tipoRouter.endStateChange();
-        });;
+          return response;
+        });
       }
      }
 
