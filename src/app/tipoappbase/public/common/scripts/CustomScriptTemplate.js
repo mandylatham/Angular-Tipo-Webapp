68e232347ce4b7b70c1c291ac913f101ff89e75c
mandylatham <mandylthm@gmail.com>
2017-09-09 15:01:47 +0300

    Merge with develop

--- src/app/tipoappbase/public/common/scripts/CustomScriptTemplate.js
+++ src/app/tipoappbase/public/common/scripts/CustomScriptTemplate.js
@@ -1,9 +1,9 @@
 (function() {
-
-  'use strict';
-
-  // Custom scripts generated
- function TipoClientJavascript(tipoHandle) {} 
- var module = angular.module('tipo.custom.gen',[]) 
- .service('tipoClientJavascript', TipoClientJavascript); 
-})();
+  
+    'use strict';
+  
+    // Custom scripts generated
+   function TipoClientJavascript(tipoHandle) {} 
+   var module = angular.module('tipo.custom.gen',[]) 
+   .service('tipoClientJavascript', TipoClientJavascript); 
+  })();
