7341d594db9fc53a271c217bee46b7ececa60f2b
mandylatham <mandylthm@gmail.com>
2017-11-27 12:15:46 +0530

    Added Tipoapp Version stamp

--- src/app/common/backend/_scripts/TipoResource.js
+++ src/app/common/backend/_scripts/TipoResource.js
@@ -291,7 +291,7 @@
                     if (_.startsWith(config.url, "g/") && $rootScope.cdn_host && !$templateCache.get(config.url + "?version_stamp=" + config.params.version_stamp)) {
                         if (_.endsWith(config.url,"___TipoApp") || _.endsWith(config.url,"___TipoDefinition") || _.startsWith($stateParams.perspective,"TipoApp.")) {
                             config.url = "https://" + $rootScope.only_cdn_host + config.url;
-                            config.params.version_stamp = $rootScope.tipoapp_version;
+                            config.params.version_stamp = $rootScope.tipoapp_version || config.params.version_stamp;
                         }else{
                             config.url = "https://" + $rootScope.cdn_host + config.url;
                         }
