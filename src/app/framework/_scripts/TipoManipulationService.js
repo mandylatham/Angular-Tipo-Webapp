250d39e51754756dad55fa1a1fb04508fffcfb18
mandylatham <mandylthm@gmail.com>
2017-12-17 16:55:53 +0530

    Fixed Menu item

--- src/app/framework/_scripts/TipoManipulationService.js
+++ src/app/framework/_scripts/TipoManipulationService.js
@@ -610,7 +610,7 @@
                     menuItem.tipo_name = parts[1];
                     var typelabel = each.type__labels || each.type_;
                     var types = typelabel.split(" - ");
-                    var type = each.tipo_type || types[1] || each.type_;
+                    var type = each.tipo_type || each.type_;
                     if (type || types[1]) {
                         var tipo_type = type || types[1].split(",");
                         _.each(tipo_type,function(each_type){
