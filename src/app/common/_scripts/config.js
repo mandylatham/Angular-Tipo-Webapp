1de01bd8980aa6fdbb7a5c390820dbb311c03a06
mandylatham <mandylthm@gmail.com>
2018-02-23 15:38:02 +0500

    update Config

--- src/app/common/_scripts/config.js
+++ src/app/common/_scripts/config.js
@@ -1,8 +1,8 @@
 var TIPO_CONSTANTS = {
   COGNITO: {
           REGION: 'us-east-2',
-          IDENTITY_POOL_ID: 'us-east-2:a3c174bb-474c-467b-b48f-10b5c451d242',
-          USER_POOL_ID: 'us-east-2_CQjpCRVSL',
-          CLIENT_ID: '7kv4t8q0r5i8tmnjpl4lv73gde'  
+          IDENTITY_POOL_ID: 'us-east-2:2aeacf33-b69e-407f-8d90-88ca4d278131',
+          USER_POOL_ID: 'us-east-2_aW68lUeCI',
+          CLIENT_ID: '2i4l70sbteu4gjr85qfpqac5cl'  
   }
 };
