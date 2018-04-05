bd21de5172f8a004a9e514ebb82f069ab688dea8
mandylatham <mandylthm@gmail.com>
2018-04-05 18:23:42 +0530

    Fixed Issues of visualisation

--- src/app/framework/_directives/_scripts/tpD3.js
+++ src/app/framework/_directives/_scripts/tpD3.js
@@ -12,7 +12,7 @@
                 xaxis: "=",
                 yaxis: "=",
                 tipoName: "@",
-                spiltChart: "=",
+                splitChart: "=",
                 height: "=",
                 margin: "=",
                 x_label: "=",
@@ -25,17 +25,12 @@
             link: function(scope, element, attrs, ctrl) {
                 var graph = (scope.yaxis.metric && { bucket_name: 'xAxis', metric_name: 'yAxis', bucket: scope.xaxis, metric: scope.yaxis }) || { metric_name: 'xAxis', bucket_name: 'yAxis', metric: scope.xaxis, bucket: scope.yaxis };
                 var fieldName = graph.bucket.field_name;
-                if (graph.bucket.aggregration_type === "Terms") {
-                    var aggegration_type = "terms";
-                    fieldName = fieldName + ".keyword";
-                } else if (graph.bucket.aggregration_type === "Date Histogram") {
-                    var aggegration_type = "date_histogram";
-                } else if (graph.bucket.aggregration_type === "Range") {
-                    var aggegration_type = "range";
-                };
+                scope.field_label = {};
                 var filter = {};
-                var field_name = fieldName.replace(/\./g, "_");
-                _.set(filter, "tipo_aggs." + field_name + "." + aggegration_type + ".field", fieldName);
+                _.set(filter, "tipo_aggs", getElasticQuery(graph.bucket, {}, "bucket"));
+                if (!_.isEmpty(scope.splitChart.field_name)) {
+                    _.set(filter, "tipo_aggs." + _.get(scope.field_label, "bucket") + ".aggs", getElasticQuery(scope.splitChart, {}, "splitChart"));
+                };
                 tipoInstanceDataService.aggegrationData(scope.tipoName, filter).then(function(results) {
                     console.log("results");
                     console.log(results);
@@ -49,7 +44,7 @@
                             $mdColorUtil.rgbaToHex($mdColors.getThemeColor('primary-400')),
                             $mdColorUtil.rgbaToHex($mdColors.getThemeColor('accent-200')),
                             $mdColorUtil.rgbaToHex($mdColors.getThemeColor('primary-200')),
-                            ],
+                        ],
                         tooltip: {
                             trigger: 'axis',
                             axisPointer: {
@@ -62,7 +57,7 @@
                         calculable: true,
                     };
                     _.each(results, function(result, index) {
-                        var dataValues = _.get(result, field_name + ".buckets");
+                        var dataValues = _.get(result, _.get(scope.field_label, "bucket") + ".buckets");
                         if (scope.chartType === "PieChart") {
                             option = getPieChartOption(dataValues, index, option)
                         } else {
@@ -71,6 +66,7 @@
                             } else if (scope.chartType === "LineChart") {
                                 var chart_type = "line";
                             };
+                            var aggegration_type = scope.aggegration_type["splitChart"] || scope.aggegration_type["bucket"]
                             if (aggegration_type === "date_histogram") {
                                 option = getTimeOption(dataValues, index, option, chart_type)
                             } else {
@@ -100,6 +96,25 @@
                 // use configuration item and data specified to show chart
                 var myChart = echarts.init(ndWrapper);
 
+
+                function getElasticQuery(bucket, aggs, type) {
+                    var field_label = bucket.field_name.replace(/\./g, "_");
+                    _.set(scope.field_label, type, field_label);
+                    if (bucket.aggregration_type === "Terms") {
+                        _.set(scope, "aggegration_type." + type, "terms");
+                        bucket.field_name = bucket.field_name;
+                        _.set(aggs, field_label + "." + scope.aggegration_type[type] + ".field", bucket.field_name);
+                    } else if (bucket.aggregration_type === "Date Histogram") {
+                        _.set(scope, "aggegration_type." + type, "date_histogram");
+                        _.set(aggs, field_label + "." + scope.aggegration_type[type] + ".field", bucket.field_name);
+                        _.set(aggs, field_label + "." + scope.aggegration_type[type] + ".interval", bucket.interval);
+                    } else if (bucket.aggregration_type === "Range") {
+                        _.set(scope, "aggegration_type." + type, "range");
+                        _.set(aggs, field_label + "." + scope.aggegration_type[type] + ".field", bucket.field_name);
+                    };
+                    return aggs;
+                }
+
                 function getPieChartOption(dataValues, index, option) {
                     _.set(option, "series[" + index + "].type", "pie");
                     _.set(option, "series[" + index + "].data", []);
@@ -112,30 +127,76 @@
                 function getxyAxisOption(dataValues, index, option, chart_type) {
                     _.set(option, graph.bucket_name + "[" + index + "].type", "category");
                     _.set(option, graph.metric_name + "[" + index + "].type", "value");
-                    _.set(option, graph.bucket_name + "[" + index + "].data", []);
                     _.set(option, "series[" + index + "].type", chart_type);
                     _.set(option, "series[" + index + "].data", []);
                     _.each(dataValues, function(each) {
-                        option[graph.bucket_name][index].data.push(each.key);
-                        option.series[index].data.push(each.doc_count);
+                        if (!_.isEmpty(scope.splitChart.field_name)) {
+                            option.legend.data.push(each.key);
+                            _.set(option, "series[" + index + "].name", each.key);
+                            var splitChartValues = _.get(each, _.get(scope.field_label, "splitChart") + ".buckets", index);
+                            _.each(splitChartValues, function(each_value) {
+                                option = getSeriesData(option, each, index);
+                            })
+                        } else {
+                            option = getSeriesData(option, each, index);
+                        }
                     });
                     return option;
                 }
 
+                function getSeriesData(option, each, index) {
+                    if (graph.bucket_name === "xAxis") {
+                        option.series[index].data.push({
+                            value: [each.key_as_string || each.key, each.doc_count]
+                        });
+                    } else {
+                        option.series[index].data.push({
+                            value: [each.doc_count, each.key_as_string || each.key]
+                        });
+                    }
+                    return option;
+                }
+
                 function getTimeOption(dataValues, index, option, chart_type) {
-                    _.set(option, "xAxis[" + index + "].type", "time");
-                    _.set(option, "yAxis[" + index + "].type", "value");
+                    _.set(option, graph.bucket_name + "[" + index + "].type", "time");
+                    _.set(option, graph.metric_name + "[" + index + "].type", "value");
                     _.set(option, "series[" + index + "].type", chart_type);
                     _.set(option, "series[" + index + "].data", []);
                     _.each(dataValues, function(each) {
                         var date_value = new Date(each.key_as_string);
+                        if (!_.isEmpty(scope.splitChart.field_name)) {
+                            option.legend.data.push(each.key);
+                            var splitChartValues = _.get(each, _.get(scope.field_label, "splitChart") + ".buckets", index);
+                            _.each(splitChartValues, function(each_value,index) {
+                                _.set(option, "series[" + index + "].data", []);
+                                _.set(option, "series[" + index + "].type", chart_type);
+                                _.set(option, "series[" + index + "].name", each.key);
+                                option = getTimeSeriesData(option, each_value, index);
+                            })
+                        } else {
+                            option = getTimeSeriesData(option, each, index);
+                        }
+                    });
+                    return option;
+                }
+
+                function getTimeSeriesData(option, each, index) {
+                    var date_value = new Date(each.key_as_string);
+                    if (graph.bucket_name === "xAxis") {
                         option.series[index].data.push({
                             name: each.key_as_string,
                             value: [
                                 [date_value.getFullYear(), date_value.getMonth() + 1, date_value.getDate()].join('/'), each.doc_count
                             ]
                         });
-                    });
+                    } else {
+                        option.series[index].data.push({
+                            name: each.key_as_string,
+                            value: [
+                                each.doc_count, [date_value.getFullYear(), date_value.getMonth() + 1, date_value.getDate()].join('/')
+                            ]
+                        });
+                    }
                     return option;
                 }
             }
